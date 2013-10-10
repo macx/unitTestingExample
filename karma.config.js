31dec1a230dc3aa02a9c56fb87985fb21d469b99
Ryan Marples <rmarples@gmail.com>
2013-10-10 09:24:00 -0700

    Revert "Made Karma run just once"
    This reverts commit c901194416c62a751a2b26b801401a6640612fc0.

--- karma.config.js
+++ karma.config.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // Start these browsers, currently available:
@@ -67,6 +67,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: true
+    singleRun: false
   });
 };
